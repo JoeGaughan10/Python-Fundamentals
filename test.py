c24a93c328ee36fbe30aa0f9baa23aca43d6ed3e
Joseph Gaughan <joe.s.gaughan@gmail.com>
2024-10-29 17:14:27 +0000

    first commit

---
+++ test.py
@@ -0,0 +1,19 @@
+#TASK 1
+
+# print("   |   |   ")
+# print("-----------")
+# print("   |   |   ")
+# print("-----------")
+# print("   |   |   ")
+
+# TASK 2
+# a)
+
+print("Hello World (lower)".lower())
+print("Hello World (capitalize)".capitalize())
+print("Hello World (count)".count("Hello"))
+print("Hello World (find)".find("World"))
+print("Hello Worl (replace)".replace("World", "Universe"))
+print("Hello World (strip)".strip())
+
+
